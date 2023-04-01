529649126ae8cf7f6380f205c1883889ffbd9391
Vinicius Sylvestre <vinicius.sylvestre@hypeone.com>
2023-04-01 17:48:33 -0300

    tryout batchs

---
+++ UNET_TensorFlow_v3.py
@@ -0,0 +1,195 @@
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import os
+import cv2
+from complement_functions import *
+
+import numpy as np 
+import os
+import skimage.io as io
+import skimage.transform as trans
+import numpy as np
+from keras.models import *
+from keras.layers import *
+from keras.optimizers import *
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler,CSVLogger
+from keras import backend as keras
+from keras.backend import epsilon
+
+
+# PARAMETERS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+NAME_MODEL = 'unet_model_batches_test'
+
+IMG_HEIGHT = 256
+IMG_WIDTH= 256
+IMG_CHANNELS = 3
+num_classes = 1
+
+EPOCHS = 4
+BATCH_SIZE = 4
+
+# PARAMETERS <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+# LIMIT GPU >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+# gpus = tf.config.list_physical_devices('GPU')
+# if gpus:
+#   # Restrict TensorFlow to only allocate 1GB of memory on the first GPU
+#   try:
+#     tf.config.set_logical_device_configuration(
+#         gpus[0],
+#         [tf.config.LogicalDeviceConfiguration(memory_limit=1024*4)])
+#     logical_gpus = tf.config.list_logical_devices('GPU')
+#     print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+#   except RuntimeError as e:
+#     # Virtual devices must be set before GPUs have been initialized
+#     print(e)
+
+# LIMIT GPU <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+# LOAD DATA >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+model_path = "./models/{}.h5".format(NAME_MODEL)
+images_train, images_test, masks_train, masks_test = get_folders(['CVC-ClinicDB','Kvasir-recortado','Children_NoPolip',
+                                                                  'sessile-main-Kvasir-SEG','Kvasir-SEG'],0.1)
+
+X = get_files(images_train,type_of_file='image')
+y = get_files(masks_train,type_of_file='mask')
+X_v = get_files(images_test,type_of_file='image')
+y_v = get_files(masks_test,type_of_file='mask')
+
+dataset_train = tf.data.Dataset.from_tensor_slices((X, y))
+dataset_val = tf.data.Dataset.from_tensor_slices((X_v, y_v))
+
+dataset_train = dataset_train.shuffle(buffer_size=len(X)).batch(BATCH_SIZE)
+dataset_val = dataset_val.batch(BATCH_SIZE)
+
+# LOAD DATA <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+# LAYERS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+inputs = tf.keras.layers.Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
+
+#Contraction path
+c1 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(inputs)
+c1 = tf.keras.layers.Dropout(0.1)(c1)
+c1 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
+b1 = tf.keras.layers.BatchNormalization()(c1)
+r1 = tf.keras.layers.ReLU()(b1)
+p1 = tf.keras.layers.MaxPooling2D((2, 2))(r1)
+
+c2 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p1)
+c2 = tf.keras.layers.Dropout(0.1)(c2)
+c2 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
+b2 = tf.keras.layers.BatchNormalization()(c2)
+r2 = tf.keras.layers.ReLU()(b2)
+p2 = tf.keras.layers.MaxPooling2D((2, 2))(r2)
+ 
+c3 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p2)
+c3 = tf.keras.layers.Dropout(0.2)(c3)
+c3 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
+b3 = tf.keras.layers.BatchNormalization()(c3)
+r3 = tf.keras.layers.ReLU()(b3)
+p3 = tf.keras.layers.MaxPooling2D((2, 2))(r3)
+ 
+c4 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p3)
+c4 = tf.keras.layers.Dropout(0.2)(c4)
+c4 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
+b4 = tf.keras.layers.BatchNormalization()(c4)
+r4 = tf.keras.layers.ReLU()(b4)
+p4 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(r4)
+ 
+c5 = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p4)
+b5 = tf.keras.layers.BatchNormalization()(c5)
+r5 = tf.keras.layers.ReLU()(b5)
+c5 = tf.keras.layers.Dropout(0.3)(r5)
+c5 = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
+
+#Expansive path 
+u6 = tf.keras.layers.Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c5)
+u6 = tf.keras.layers.concatenate([u6, c4])
+u6 = tf.keras.layers.BatchNormalization()(u6)
+u6 = tf.keras.layers.ReLU()(u6)
+
+
+u7 = tf.keras.layers.Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(u6)
+u7 = tf.keras.layers.concatenate([u7, c3])
+u7 = tf.keras.layers.BatchNormalization()(u7)
+u7 = tf.keras.layers.ReLU()(u7)
+
+ 
+u8 = tf.keras.layers.Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(u7)
+u8 = tf.keras.layers.concatenate([u8, c2])
+u8 = tf.keras.layers.BatchNormalization()(u8)
+u8 = tf.keras.layers.ReLU()(u8)
+ 
+u9 = tf.keras.layers.Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(u8)
+u9 = tf.keras.layers.concatenate([u9, c1], axis=3)
+u9 = tf.keras.layers.BatchNormalization()(u9)
+u9 = tf.keras.layers.ReLU()(u9)
+
+
+outputs = tf.keras.layers.Conv2D(num_classes, (1, 1), activation='sigmoid')(u9)
+# outputs = tf.keras.layers.Lambda(apply_threshold)(outputs)
+outputs = ThresholdLayer()(outputs)
+
+# LAYERS <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+# CALLBACKS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+history_logger=tf.keras.callbacks.CSVLogger("./history/{}.csv".format(NAME_MODEL), separator=",", append=True)
+
+model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+    filepath='./tmp/checkpoint',
+    save_weights_only=True,
+    monitor='val_loss',
+    mode='min',
+    save_best_only=True)
+
+callbacks = [
+        tf.keras.callbacks.EarlyStopping(patience=5, monitor='val_loss'),
+        tf.keras.callbacks.TensorBoard(log_dir='logs'),
+        history_logger,
+        model_checkpoint_callback]
+
+# CALLBACKS <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+# CREATE OR LOAD MODEL >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+if os.path.isfile(model_path):
+    # model = tf.keras.models.load_model(model_path)
+    model = tf.keras.models.load_model(model_path,custom_objects={'dice_loss':dice_loss,'ThresholdLayer': ThresholdLayer})
+
+else:
+    model = tf.keras.Model(inputs=[inputs], outputs=[outputs])
+    model.compile(optimizer = Adam(lr = 1e-4), loss = dice_loss, metrics = ['accuracy'])
+
+# CREATE OR LOAD MODEL <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+# TRAIN >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+# history = model.fit(X, y, validation_data=(X_v,y_v), batch_size=BATCH_SIZE, epochs=EPOCHS, callbacks=callbacks)
+history = model.fit(dataset_train, epochs=EPOCHS, validation_data=dataset_val, callbacks=callbacks)
+
+# TRAIN <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+# SAVE >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+model.load_weights('./tmp/checkpoint')
+print(model.summary())
+print(history)
+
+model.save(model_path)
+# np.save('./history/{}.npy'.format(NAME_MODEL),history.history)
+print('Model Saved!')
+
+# SAVE <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
